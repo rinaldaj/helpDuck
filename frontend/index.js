b1a2aec23b43ae248f6966cbbf030ca98a1cd984
Jacob Burritt <burritjr@clarkson.edu>
2018-11-18 04:39:37 -0500

    j s o n

--- frontend/index.js
+++ frontend/index.js
@@ -34,9 +34,7 @@ function placeMarker(address){
 function grabJSON(){
     fetch('http://35.227.91.78/service')
   .then(function(response) {
-    var obj = JSON.parse(response);
-    placeMarker(obj.Address);
-    return obj;
+    return JSON.parse(response.json());
   })
   .then(function(myJson) {
     console.log(JSON.stringify(myJson));
